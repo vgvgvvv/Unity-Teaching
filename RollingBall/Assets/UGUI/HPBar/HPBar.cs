ec55a7a55f7e4afb67ab881265e721d35ea5ab35
DongChen <352070977@qq.com>
2019-02-17 16:59:59 +0800

    更新

---
+++ RollingBall/Assets/UGUI/HPBar/HPBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPBar : MonoBehaviour
+{
+
+	public GameObject target;
+	public Camera mainCamera;
+	public Camera uiCamera;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target == null)
+		{
+			return;
+		}
+		
+		Vector3 screenPos = mainCamera.WorldToScreenPoint(target.transform.position + Vector3.up * 1);
+		Vector3 uiSpaceWorldPos = uiCamera.ScreenToWorldPoint(screenPos);
+		transform.position = uiSpaceWorldPos;
+		transform.LookAt(uiCamera.transform);
+		
+	}
+}
