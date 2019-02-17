63df858f8c01c5df10475475b53fb63657cc54b7
Dong Chen <352070977@qq.com>
2019-02-17 19:50:28 +0800

    添加模型以及贴图资源

---
+++ RollingBall/Assets/Animation/Script/MAnimator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MAnimator
+{
+
+	private Animator _animator;
+	private AnimatorOverrideController _overrideController;
+
+	public void Init(GameObject go)
+	{
+		_animator = go.GetComponent<Animator>();
+		_overrideController = new AnimatorOverrideController();
+		_animator.runtimeAnimatorController = _overrideController;
+		
+	}
+
+}
---
+++ RollingBall/Assets/Animation/Script/TestAnimator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestAnimator : MonoBehaviour
+{
+	private MAnimator _animator;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		_animator = new MAnimator();
+		_animator.Init(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
